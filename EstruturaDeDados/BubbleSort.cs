2efe50c0bae6d563269ab8ef757876a2e590a2dc
immerfallen <marodemelo10@gmail.com>
2023-09-26 08:52:35 -0300

    Add project files.

---
+++ EstruturaDeDados/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeDados
+{
+    public class BubbleSort
+    {
+
+        public int[] OrdenacaoBubbleSort(int[] array)
+        {
+            var tamanho = array.Length;
+            var comparacoes = 0;
+            var trocas = 0;
+
+            for (int i = tamanho - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    comparacoes++;
+                    if (array[j] > array[j + 1])
+                    {
+                        int aux = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = aux;
+                        trocas++;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+    
+}
